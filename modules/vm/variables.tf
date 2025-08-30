e5c7f8e42c073d30b22374a5669f5ee77044de3a
Aulia Wicaksono <aulia.wicaksono@gojek.com>
2025-08-30 21:12:21 +0700

    fix modules

--- modules/vm/variables.tf
+++ modules/vm/variables.tf
@@ -1,5 +1,24 @@
-variable "cpu"       { type = number, default = 2 }
-variable "memory_gb" { type = number, default = 4 }
-variable "disk_gb"   { type = number, default = 50 }
-variable "public_ip" { type = bool,   default = false }
-variable "os"        { type = string, default = "ubuntu-22.04" }
+variable "cpu" {
+  type    = number
+  default = 2
+}
+
+variable "memory_gb" {
+  type    = number
+  default = 4
+}
+
+variable "disk_gb" {
+  type    = number
+  default = 50
+}
+
+variable "public_ip" {
+  type    = bool
+  default = false
+}
+
+variable "os" {
+  type    = string
+  default = "ubuntu-22.04"
+}
